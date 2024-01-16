32415b3d7b3979635d4b072f86bf01eb517aa08e
Devesh <67568465+deveshksrivastava@users.noreply.github.com>
2024-01-16 11:38:32 +0000

    fix country issue

--- src/valuable/Interview-country-state.tsx
+++ src/valuable/Interview-country-state.tsx
@@ -1,49 +1,80 @@
 import React, { useState } from 'react';
+import { countries } from './../valuable/data/countries';
+// create json object for country state and city data
 
-const InterviewCountryState: React.FC = () => {
+export default function App() {
+  const [countryList, setCountryList] = useState(countries);
   const [selectedCountry, setSelectedCountry] = useState('');
   const [selectedState, setSelectedState] = useState('');
-  const [selectedCity, setSelectedCity] = useState('');
+  const [selectedCity, setselectedCity] = useState('');
 
   const handleCountryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedCountry(event.target.value);
+    const newArr = countryList.filter(
+      item => item.country === event.target.value,
+    );
+    setCountryList(newArr);
   };
+
+  // how to add two number
 
   const handleStateChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedState(event.target.value);
+    console.log('state :', event.target.value);
+    const newArr = countryList.filter(item => {
+      item.states.filter(item => item.state === event.target.value);
+    });
+    console.log('state :', newArr);
+    setCountryList(newArr);
+    // setSelectedState(event.target.value)
   };
-
-  const handleCityChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedCity(event.target.value);
-  };
-
   return (
     <div>
-      <h1>Country, State, and City Selection</h1>
-
-      <label htmlFor="country">Country:</label>
       <select
+        name="country"
         id="country"
         value={selectedCountry}
         onChange={handleCountryChange}
       >
-        <option value="">Select a country</option>
-        {/* Add your country options here */}
+        <option value="India">--country--</option>
+        {countryList.map(item => {
+          return <option value={item.country}>{item.country}</option>;
+        })}
       </select>
-
-      <label htmlFor="state">State:</label>
-      <select id="state" value={selectedState} onChange={handleStateChange}>
-        <option value="">Select a state</option>
-        {/* Add your state options here */}
+      <select
+        name="state"
+        id="state"
+        value={selectedState}
+        onChange={handleStateChange}
+      >
+        <option value="India">--state--</option>
+        {countryList.map(item => {
+          return (
+            <>
+              {item.states.map(state => {
+                return <option value={state.state}>{state.state}</option>;
+              })}
+            </>
+          );
+        })}
       </select>
-
-      <label htmlFor="city">City:</label>
-      <select id="city" value={selectedCity} onChange={handleCityChange}>
-        <option value="">Select a city</option>
-        {/* Add your city options here */}
+      <select name="city" id="city">
+        <option value="India">--city--</option>
+        {countryList.map(item => {
+          return (
+            <>
+              {item.states.map(state => {
+                return (
+                  <>
+                    {state.cities.map(city => {
+                      return <option value="city">{city}</option>;
+                    })}
+                  </>
+                );
+              })}
+            </>
+          );
+        })}
       </select>
+      {JSON.stringify(countries)}
     </div>
   );
-};
-
-export default InterviewCountryState;
+}
