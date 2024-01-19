5ab3679ec77fe1ae101fb242d6b4662b498b55e5
SatishKumar <53410112+Satish-KumarGupta@users.noreply.github.com>
2024-01-19 21:45:36 +0530

    Dropdown fixing (#8)
    * fixing on navbar
    
    * fix model view and contact and help page
    
    * make dropdown navbar
    
    * remove duplicate code

--- src/valuable/service.tsx
+++ src/valuable/service.tsx
@@ -3,14 +3,6 @@ import Banner from './pages/banner';
 const Services = () => {
   return (
     <div className="min-h-screen  w-full">
-      <section className='text-center underline cursor-pointer py-5 text-red-500'>
-        <div className='p-2 '>
-        Request Advanced Analytics Enviornment
-        </div>
-        <div className='p-2'>
-        Request data Upload 
-        </div>
-      </section>
       <Banner />
     </div>
   );
