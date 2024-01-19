09274fb88110da0c553679f4aaa170edb2941903
SatishKumar <53410112+Satish-KumarGupta@users.noreply.github.com>
2024-01-19 13:00:24 +0530

    Menu fixing on navbar (#4)
    * fixing on navbar
    
    ------------------

--- src/valuable/Header.tsx
+++ src/valuable/Header.tsx
@@ -1,27 +1,41 @@
 import { Link } from 'react-router-dom';
 
+import NavLinkTag from './pages/nav-link';
+const navLinks = [
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'Service',
+    path: '/service'
+  },
+  {
+    title: 'Help',
+    path: '/help'
+  },
+  {
+    title: 'Contact Us',
+    path: '/contact'
+  }
+];
 const Header = () => {
   return (
-    <div className=" justify-between bg-slate-100 shadow-md h-15">
+    <div className=" justify-between bg-slate-100 shadow-md h-15 ">
       <div className="flex items-start max-w-7xl px-2 py-2">
         <img src="/asserts/img/logo.png" className="w-20" alt="Logo" />
       </div>
-      <div className="flex bg-blue-400  justify-center items-center">
-        <ul className="flex gap-4 no-underline ">
-          <li className="text-white  p-2 ">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="text-white  p-2 ">
-            <Link to="/service">Service</Link>
-          </li>
-          <li className="text-white  p-2 ">
-            <Link to="/help">Help</Link>
-          </li>
-          <li className="text-white  p-2 ">
-            <Link to="/contact">Contact Me</Link>
-          </li>
+
+      <div className=" flex bg-red-400 px-2 py-2  justify-center items-center" id="navbar">
+        <ul className="flex md:p-0 md:flex-row md:space-x-8 mt-0 mb-[3px] ">
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <NavLinkTag href={link.path} title={link.title} />
+            </li>
+          ))}
         </ul>
-      </div>
+        </div>
+
       <div>
         <nav className="flex" aria-label="Breadcrumb">
           <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
