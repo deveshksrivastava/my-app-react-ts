5964b4f7fc2b325aca63b848a05dec6e1f41b1d6
Devesh Srivastava <devesh.kr.sri@gmail.com>
2024-11-25 22:26:31 +0000

    add shad cn

--- src/2024-June_Interview-prep/performance/skills/country-state.tsx
+++ src/2024-June_Interview-prep/performance/skills/country-state.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { countries } from '../../../valuable/data/countries';
+import { Button, buttonVariants } from "../../../components/ui/button";
+import { Link } from 'react-router-dom';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../../components/ui/tabs";
 // create json object for country state and city data
 
 export default function CoutryStateDDl() {
@@ -20,32 +23,46 @@ export default function CoutryStateDDl() {
   };
   return (
     <div className="text-center m-4">
-      <select name="country" id="country" value={selectedCountry} onChange={handleCountryChange}>
-        <option value="">--country--</option>
-        {countries.map(item => (
-          <option key={item.country} value={item.country}>
-            {item.country}
-          </option>
-        ))}
-      </select>
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList>
+          <TabsTrigger value="shadcn">ShadCN</TabsTrigger>
+          <TabsTrigger value="ddl">DDL Country State</TabsTrigger>
+        </TabsList>
+        <TabsContent value="shadcn">
+          <Button variant="destructive">Button</Button>
+          <Link to='/' className={buttonVariants({ variant: "outline" })}>Click here</Link>
+        </TabsContent>
+        <TabsContent value="ddl">
+          <select name="country" id="country" value={selectedCountry} onChange={handleCountryChange}>
+            <option value="">--country--</option>
+            {countries.map(item => (
+              <option key={item.country} value={item.country}>
+                {item.country}
+              </option>
+            ))}
+          </select>
 
-      <select name="state" id="state" value={selectedState} onChange={handleStateChange}>
-        <option value="">--state--</option>
-        {fetchstate?.states.map(state => (
-          <option key={state.state} value={state.state}>
-            {state.state}
-          </option>
-        ))}
-      </select>
+          <select name="state" id="state" value={selectedState} onChange={handleStateChange}>
+            <option value="">--state--</option>
+            {fetchstate?.states.map(state => (
+              <option key={state.state} value={state.state}>
+                {state.state}
+              </option>
+            ))}
+          </select>
 
-      <select name="city" id="city">
-        <option value="">--city--</option>
-        {fetchcity?.cities.map((city: string) => (
-          <option key={city} value={city}>
-            {city}
-          </option>
-        ))}
-      </select>
+          <select name="city" id="city">
+            <option value="">--city--</option>
+            {fetchcity?.cities.map((city: string) => (
+              <option key={city} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+        </TabsContent>
+      </Tabs>
+
+
     </div>
   );
 }
